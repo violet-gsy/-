f29a6509441a17d694e13ec74b5518c838baa2f4
DESKTOP-UP48D0M\wan <wyz@test.com>
2023-02-10 18:40:54 +0800

    v0.2修正调试接口

--- src/main/java/com/cetc28/needmanagement/MainApplication.java
+++ src/main/java/com/cetc28/needmanagement/MainApplication.java
@@ -1,8 +1,14 @@
 package com.cetc28.needmanagement;
 
+import com.cetc28.needmanagement.entity.CompanyDct;
+import com.cetc28.needmanagement.service.CompanyDctService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.cetc28.needmanagement.mapper")
@@ -10,5 +16,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class);
+
+
+
+
     }
 }
--- src/main/java/com/cetc28/needmanagement/controller/CompanyDctController.java
+++ src/main/java/com/cetc28/needmanagement/controller/CompanyDctController.java
@@ -4,12 +4,15 @@ import com.cetc28.needmanagement.entity.CompanyDct;
 import com.cetc28.needmanagement.service.CompanyDctService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/companyDct")
 public class CompanyDctController {
     @Autowired
@@ -18,7 +21,14 @@ public class CompanyDctController {
 
     @GetMapping("/selCompanyDct")
     public List<CompanyDct> selCompanyDct(){
+
         return companyDctService.list();
+    }
+
+    @GetMapping("/count")
+    public Long selCompanyDct1(){
+        return 5L;
+//        return companyDctService.count();
     }
 
 }
