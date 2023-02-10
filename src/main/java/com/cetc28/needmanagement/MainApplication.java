e27ef25bb3ff986ec2bb6ac3d7822cc3e5c3ac15
DESKTOP-UP48D0M\wan <wyz@test.com>
2023-02-10 18:26:48 +0800

    v0.2创建测试接口

--- src/main/java/com/cetc28/needmanagement/MainApplication.java
+++ src/main/java/com/cetc28/needmanagement/MainApplication.java
@@ -1,9 +1,11 @@
 package com.cetc28.needmanagement;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cetc28.needmanagement.mapper")
 public class MainApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cetc28/needmanagement/controller/CompanyDctController.java
+++ src/main/java/com/cetc28/needmanagement/controller/CompanyDctController.java
@@ -4,6 +4,7 @@ import com.cetc28.needmanagement.entity.CompanyDct;
 import com.cetc28.needmanagement.service.CompanyDctService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class CompanyDctController {
     CompanyDctService companyDctService;
 
 
-    @RequestMapping("/selCompanyDct")
+    @GetMapping("/selCompanyDct")
     public List<CompanyDct> selCompanyDct(){
         return companyDctService.list();
     }
